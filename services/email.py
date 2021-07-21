5614d9e1ff100cfbda1db2b73b82378097266311
Kali Junior Brown <browndesmond30@yahoo.com>
2021-07-21 23:55:42 +0000

    Modified email sending just for sanity checks on pythonanywhere

--- services/email.py
+++ services/email.py
@@ -7,10 +7,10 @@ def sender():
     """
     Send a test email
     """
-    subject = "Python Anywhere Async Email Test"
+    subject = "Finally Done"
     message = "Currently, Python anwhere does not support asynchronous tasks using thread. This is a test to send email asynchronously your RQ"
     email_from = "info@test.com"
-    recipient_list = ["browndesmond30@yahoo.com"]
+    recipient_list = ["desmond@gradia.net", "browndesmond30@yahoo.com"]
 
     for _ in range(5):
         send_mail(subject=subject, message=message, from_email=email_from, recipient_list=recipient_list)
